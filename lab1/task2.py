f0b92e270fbb78aa5f78e3292e239893a3dd638a
KissMe2705 <nazarmehonoshin@gmail.com>
2025-12-16 22:37:16 +0500

    Update task2.py

--- lab1/task2.py
+++ lab1/task2.py
@@ -1,1 +1,11 @@
+from math import cos, log, sin, exp, sqrt
 
+x = float(input("Введите значение переменной x: "))
+y = float(input("Введите значение переменной y: "))
+z = float(input("Введите значение переменной z: "))
+
+a = sqrt(z**3 / 2) - sin(y)
+b = exp(2) / 3 - cos(y) + z + log(y)
+
+print(f"Получено значение функции a = {a}")
+print(f"Получено значение функции b = {b}")
